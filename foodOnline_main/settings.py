2c470789b76db4e0ded1f3ad80b5c8ab4bdab305
kajal <kajalpoojari555@gmail.com>
2024-07-18 01:54:51 +0530

    forgot password feature

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -141,3 +141,18 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# # Email configuration
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kajalpoojari1996@gmail.com'
+EMAIL_HOST_PASSWORD = 'uwxzipklgfiojtvc'
+DEFAULT_FROM_EMAIL = 'FoodOnline Marketplace <kajalpoojari1996@gmail.com>'
