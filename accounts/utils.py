1603bbd1b590e4a929476fac89cf07e41e0e6158
kajal <kajalpoojari555@gmail.com>
2024-07-20 23:06:07 +0530

    Email approvals, Vendor profile form and validations

--- accounts/utils.py
+++ accounts/utils.py
@@ -33,19 +33,14 @@ def send_verification_email(request, user, mail_subject, email_template):
     mail.send()
 
 
-   
-# def send_password_reset_email(request, user):
-#     from_email = settings.DEFAULT_FROM_EMAIL
-#     current_site = get_current_site(request)
-#     mail_subject = "Please reset your password"
-#     message = render_to_string('accounts/email/reset_password_email.html', {
-#         'user': user,
-#         'domain': current_site,
-#         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-#         'token': default_token_generator.make_token(user),
-#     })
-#     to_email = user.email
-#     mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
-#     # mail.content_subtype = "html"
-#     mail.send()
-  
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    if(isinstance(context['to_email'], str)):
+        to_email = []
+        to_email.append(context['to_email'])
+    else:
+        to_email = context['to_email']
+    mail = EmailMessage(mail_subject, message, from_email, to=to_email)
+    mail.content_subtype = "html"
+    mail.send()
